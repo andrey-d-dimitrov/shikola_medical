978277ca469a186f42891f366be1ddc66fdeaeab
andrey-d-dimitrov <andrey.d.dimitrov.2023@elsys-bg.org>
2024-05-19 19:22:05 +0300

    navbar

---
+++ static/script.js
@@ -0,0 +1,6 @@
+function toggleNavbar() {
+    var navbar = document.getElementById("navbar");
+    navbar.classList.toggle("active");
+    var navbarToggleBtn = document.getElementById("navbarToggleBtn");
+    navbarToggleBtn.style.display = "none"; 
+}
