f422af7b7b363846323874a39cc1f7762fa358c7
andrey-d-dimitrov <andrey.d.dimitrov.2023@elsys-bg.org>
2024-05-19 22:26:41 +0300

    mergeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- main.py
+++ main.py
@@ -10,13 +10,41 @@ def index():
 def home():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/doctor1')
 def about():
-    return render_template('about.html')
+    return render_template('doctor1.html')
 
-@app.route('/contact')
-def contact():
+
+@app.route('/doctor2')
+def doktor2():
     return render_template('doctor2.html')
+
+@app.route('/doctor3')
+def doktor3():
+    return render_template('doctor3.html')
+
+@app.route('/doctor4')
+def doktor4():
+    return render_template('doctor4.html')
+
+@app.route('/doctor5')
+def doktor5():
+    return render_template('doctor5.html')
+
+@app.route('/doctor6')
+def doktor6():
+    return render_template('dok6.html') 
+
+@app.route('/doctor7')
+def doktor7():
+    return render_template('dok7.html')
+
+@app.route('/doctor8')
+def doktor8():
+    return render_template('dok8.html')
+@app.route('/doctori')
+def doctori():
+    return render_template('doktori.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script13.js
+++ static/script13.js
@@ -5,50 +5,50 @@ window.onload = function() {
         {
             name: 'Д-р Мария Илиева',
             specialization: 'Акушер-гинеколог',
-            href: 'doctor1.html',
+            href: '/doctor1', // Ensure the URL matches Flask route
             image: '/static/doctor1.jpg'
         },
         {
             name: 'Д-р Стефан Шайлеков',
             specialization: 'Ортопед',
-            href: 'doctor2.html',
+            href: '/doctor2', // Ensure the URL matches Flask route
             image: '/static/doctor2.jpg'
         },
         {
             name: 'Д-р Гергана Георгиева',
             specialization: 'Образна диагностика, Мамолог',
-            href: 'doctor3.html',
+            href: '/doctor3', // Ensure the URL matches Flask route
             image: '/static/doctor3.jpg'
         },
         {
             name: 'Д-р Мая Костадинова',
             specialization: 'Ендокринолог',
-            href: 'doctor4.html',
+            href: '/doctor4', // Ensure the URL matches Flask route
             image: '/static/doctor4.jpg'
         },
         {
             name: 'Д-р Оля Ефтимова',
             specialization: 'Кардиолог',
-            href: 'doctor5.html',
-            image: '/static/doctor5.jpg'
+            href: '/doctor5', // Ensure the URL matches Flask route
+            image: 'static/92zYv6BmtiZuHIJwOQV6tqWgiRdHX6xJjKjE1hVJ.jpg'
         },
         {
             name: 'Д-р Констанца Нейкова',
             specialization: 'Акушер-гинеколог',
-            href: 'doctor6.html',
-            image: '/static/doctor6.jpg'
+            href: '/doctor6', // Ensure the URL matches Flask route
+            image: 'static/KODGyg5HlshF1Bbqq7XagAcv7wD5ynDtxG4niwXX.jpg'
         },
         {
             name: 'Д-р Мария Петришка',
             specialization: 'Невролог',
-            href: 'doctor7.html',
-            image: '/static/doctor7.jpg'
+            href: '/doctor7', // Ensure the URL matches Flask route
+            image: 'static/dXWLpnPczCrgxuAKZuclTe9YD3gvGI71tn1oURRZ.jpg'
         },
         {
             name: 'Д-р Ирена Шикова',
             specialization: 'Акушер-гинеколог',
-            href: 'doctor8.html',
-            image: '/static/doctor8.jpg'
+            href: '/doctor8', // Ensure the URL matches Flask route
+            image: 'static/czGUJgGop8zPNOBOGXWG3u0hafVgxFFPDedaVKb4.jpg'
         }
     ];
 
