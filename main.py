5b3f81eb7165c92953a8ff70632b7b45b1f8e084
andrey-d-dimitrov <andrey.d.dimitrov.2023@elsys-bg.org>
2024-05-19 21:21:32 +0300

    mergeeeeeeee

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def about():
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('doctor2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
